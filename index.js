84eae3b9bce2e4732b2584c85ffb83a83d457b5a
JeroT1 <JeroT1YT@yandex.ru>
2025-12-24 20:57:25 +0300

    6 задание

---
+++ index.js
@@ -0,0 +1,5 @@
+const userName = 'Андрей'; // const — значение не изменится
+let userAge = 25;         // let — можно изменить позже
+userAge = 26;              // изменяем значение
+
+console.log(userName, userAge);
